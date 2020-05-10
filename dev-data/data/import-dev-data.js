30c865da2816c22af3aa900c3d29ceb91dbe0c55
Polyhistor <pouya.ataei.7@gmail.com>
2020-05-10 14:39:21 +1200

    written a small bash app to update data in the database

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection established'));
+
+// READ JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// IMPORT DATA INTO DATABASE
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('data succesfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+
+  process.exit();
+};
+
+// DELETE ALL THE DATA AT THE SAME TIME
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('data succesfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);
