ae5abfdd1059b6088dca684c738b7834c58cd71c
Polyhistor <pouya.ataei.7@gmail.com>
2020-05-16 22:16:54 +1200

    aggregate middleware/hook

--- models/tourModel.js
+++ models/tourModel.js
@@ -102,6 +102,18 @@ tourSchema.post(/^find/, function (docs, next) {
   next();
 });
 
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  console.log(
+    this.pipeline().unshift({
+      $match: {
+        secretTour: { $ne: true },
+      },
+    })
+  );
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
