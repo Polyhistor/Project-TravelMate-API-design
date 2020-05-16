cbc5ab41e62fdd7bfb830b80154d4e4f342922c5
Polyhistor <pouya.ataei.7@gmail.com>
2020-05-16 20:11:17 +1200

    data aggregation pipeline practice

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,7 @@ const {
   deleteTour,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlan,
 } = require('../controllers/tourController');
 
 const router = express.Router();
@@ -14,6 +15,7 @@ const router = express.Router();
 // router.param('id', checkID);
 
 router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
