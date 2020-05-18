9a12e065fb2039bc8d0d6ae3be491cdf7e6fa37c
Polyhistor <pouya.ataei.7@gmail.com>
2020-05-18 14:28:05 +1200

    error handling for prod and dev

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,36 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: error,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // operational ,trusted
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // programming or other unknown third party issues, we do log this error for our own reference
+    console.err('Error', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'something went wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
