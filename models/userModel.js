5ed5dc6574af6d2e25dbec3355f0be4a7d020cef
Polyhistor <pouya.ataei.7@gmail.com>
2020-05-21 21:51:16 +1200

    json web token implemented

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 // name, email, photo, password, passwordConfirm
 const userSchema = mongoose.Schema({
@@ -21,8 +22,36 @@ const userSchema = mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minLength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // THIS ONLY WORK ON SAVE and CREATE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords do not match!',
+    },
   },
 });
+
+userSchema.pre('save', async function (next) {
+  // If password is not modified then do exit the function
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost salt length of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  // delete password confirm field
+  this.passwordConfirm = undefined;
+
+  next();
+});
+
+userSchema.methods.correctPassword = async function (canditatePassword, userPassword) {
+  return await bcrypt.compare(canditatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { getAllUsers, getUser, createUser, updateUser, deleteUser } = require('../controllers/userController');
-const { signUp } = require('../controllers/authController');
+const { signUp, login } = require('../controllers/authController');
 
 const userRouter = express.Router();
 
 userRouter.route('/signup').post(signUp);
+userRouter.route('/login').post(login);
+
 userRouter.route('/').get(getAllUsers).post(createUser);
 userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
