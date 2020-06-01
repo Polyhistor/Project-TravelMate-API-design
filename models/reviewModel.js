4555ccc5f20c11768cd6d2d1d78e034fde13a203
Polyhistor <pouya.ataei.7@gmail.com>
2020-06-01 14:43:53 +1200

    implemented a nested route

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,11 +34,16 @@ const reviewSchema = new mongoose.Schema(
 );
 
 reviewSchema.pre(/^find/, function (next) {
+  // this.populate({
+  //   path: 'user',
+  //   select: 'name photo',
+  // }).populate({
+  //   path: 'tour',
+  //   select: 'name photo',
+  // });
+
   this.populate({
     path: 'user',
-    select: 'name photo',
-  }).populate({
-    path: 'tour',
     select: 'name photo',
   });
 
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,6 +10,7 @@ const {
   getMonthlyPlan,
 } = require('../controllers/tourController');
 const { protect, restrictTo } = require('../controllers/authController');
+const { createReview } = require('../controllers/reviewController');
 
 const router = express.Router();
 
@@ -21,6 +22,12 @@ router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router.route('/').get(protect, getAllTours).post(createTour);
-router.route('/:id').get(getTour).patch(updateTour).delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
+router
+  .route('/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
+
+router.route('/:tourId/reviews').post(protect, restrictTo('user'), createReview);
 
 module.exports = router;
