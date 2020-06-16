133916f88809a43a81f428f058c7058a3e2086e9
Polyhistor <pouya.ataei.7@gmail.com>
2020-06-16 18:13:07 +1200

    debugged a few issues

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -17,7 +17,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     //   req.user.id
     // }&price=${tour.price}`,
 
-    success_url: `${req.protocol}://${req.get('host')}/my-tour`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourID,
@@ -25,7 +25,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: ['https://ibb.co/xCSVgqc'],
+        images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
         amount: tour.price * 100,
         currency: 'usd',
         quantity: 1,
@@ -54,7 +54,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.display_items[0].amount / 100;
 
   await Booking.create({ tour, user, price });
 };
@@ -69,7 +69,7 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`webhook error: ${err.message}`);
   }
 
-  if (event.type === 'checkout.session.complete') {
+  if (event.type === 'checkout.session.completed') {
     createBookingCheckout(event.data.object);
   }
 
